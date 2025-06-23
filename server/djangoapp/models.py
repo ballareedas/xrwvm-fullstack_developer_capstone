cc843b044741ffbd5154798ec1c7128dd6175d72
ballareedas <ballareedas9@gmail.com>
2025-06-23 05:11:22 -0400

    Add CarMake and CarModel models and register in admin

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,58 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    founded_year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.founded_year})" if self.founded_year else self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+from django.db import models
+from datetime import date
+
+# Assuming CarMake is already defined in the same or another app
+# from .models import CarMake
+
+class CarModel(models.Model):
+    # Choices for car type
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    HATCHBACK = 'Hatchback'
+    TRUCK = 'Truck'
+
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+        (TRUCK, 'Truck'),
+    ]
+
+    # many to one relationship
+    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE, related_name='models')
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    car_type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    color = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year.year})"
+
