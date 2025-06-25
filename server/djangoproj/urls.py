e0023f118eb7c54a551b9692998787c85c6339df
ballareedas <ballareedas9@gmail.com>
2025-06-25 10:01:48 -0400

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include, re_path
+
 
 
 urlpatterns = [
@@ -27,5 +29,8 @@ urlpatterns = [
         path('register/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    # path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
